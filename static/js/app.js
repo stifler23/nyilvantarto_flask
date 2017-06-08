d2cb1c6d13d64ac0f6e5058066e23a62687e44f7
stifler23 <torres922@freemail.hu>
2017-06-08 13:32:46 +0200

    Cc

--- static/js/app.js
+++ static/js/app.js
@@ -15,11 +15,117 @@
 	$("#"+clickedId+".menu-item").removeClass("btn-secondary");
 	$("#"+clickedId+".menu-item").addClass("btn-primary");
 	}
+function getToday() {
+	let now = new Date();
+	//let testDate = new Date("January 23, 2014 11:13:00");
+	let day = ('0' + now.getDate()).slice(-2);
 
+	let month = ('0' + (now.getMonth() + 1)).slice(-2);
+	let today = now.getFullYear()+"."+(month)+"."+(day);
+	return today;
+}
 
-function addMunkanap() {
+function addMunkanap() { 
+	let lastid = parseInt($(' .new_munkanap:last').attr('id'));
+
+	console.log("lastid: "+lastid);
+	if(isNaN(lastid)) {
+		lastid=0;
+	}
+	mainap = getToday();
+
 	$('#munkanapItems').append(`
-		<div>Hello</div>
+		   <div id="`+(lastid+1)+`" class="new_munkanap row">
+          <button id="`+(lastid+1)+`" class="remove btn btn-danger" onclick="removeMunkanap(this.id)">-</button>
+          <div class="col-xs-6 col-sm-12">
+            <input id="`+(lastid+1)+`" type="text" placeholder="dátum" class="datepicker datum_mezo form-control" value="`+mainap+`" >
+          </div>
+          <div class="col-xs-6 col-sm-12">
+            <input id="`+(lastid+1)+`" type="number" min="0.5" max="12" step="0.5" class="munkaora form-control" placeholder="munkaóra">
+          </div>
+          <div class="col-xs-12 col-sm-12">
+            <textarea id="`+(lastid+1)+`" rows="3" class="form-control" placeholder="megjegyzés"></textarea>
+          </div>
+        </div>
 
 		`);
+
+	$('.datepicker').datepicker({
+		language: 'hu',
+		autoclose: 'true',
+		todayBtn: 'true',
+		todayHighlight: 'true'
+
+
+	});
+
+
+
+}	//add munkanap vége
+
+
+let new_munkanap = [];
+function collectMunkanaps() {
+	console.log("munkanapok összegyűjtése......");
 }
+
+
+function removeMunkanap(munkanapToRemove) {
+	$("#"+munkanapToRemove+".new_munkanap").remove();
+	
+
+
+}
+
+function sendform() {
+	//TODO összegyűjteni
+	console.log("sending form....");
+	collectMunkanaps();
+}
+
+$(document).on('blur', '.munkaora' , function() {
+//amit ide írunk kod az akkor fut le ha a munkaora mezőt elhagyja a user
+	munkaoraMezo = parseFloat($(this).val());
+	console.log("a munkaora mezo erteke: "+munkaoraMezo);
+	console.log("a munkaora mezo tipusa: "+typeof(munkaoraMezo));
+	if ( Number.isFinite(munkaoraMezo) ) {
+	if	( munkaoraMezo > 8 ) {
+	$(this).val('8');
+		
+	} else if( munkaoraMezo < 0){
+		$(this).val('0');
+	}
+} else {
+	$(this).val('0');
+}
+
+
+
+});
+
+
+
+$(document).ready(function () {
+
+	addMunkanap();
+});
+
+
+//firefoxfix
+$(function(){
+	$("input[type='number']").on("click", function(){
+		$(this).focus();	
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
